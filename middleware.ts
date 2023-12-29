7cfe7e9cbc8e61c143cada278ec63412a5778df8
Guillaume Flambard <56681566+zoanlogia@users.noreply.github.com>
2023-12-29 22:35:36 +0100

    modified:   middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
  
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -10,4 +13,12 @@ export default authMiddleware({
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+export const middleware = (req: NextRequest) => {
+    const cookie = (req.cookies as any)["auth"];
+  
+    if (!cookie) {
+      return NextResponse.redirect("/login");
+    }
+  };
  
